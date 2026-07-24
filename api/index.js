fa9ed9a9c5cc6f0b7bbb5b81fc94963b64d3a121
Pardhanvijay069 <pardhanvijay069@gmail.com>
2026-07-25 01:43:57 +0530

    refactor: update entry point for Vercel compatibility and add diagnostic report

--- api/index.js
+++ api/index.js
@@ -1,5 +1,17 @@
 require("dotenv").config();
 
-const app = require("../src/app");
+console.log("1. index.js loaded");
 
-module.exports = app;
+try {
+    const serverless = require("serverless-http");
+    console.log("2. serverless-http loaded");
+
+    const app = require("../src/app");
+    console.log("3. app loaded");
+
+    module.exports = serverless(app);
+    console.log("4. export completed");
+} catch (err) {
+    console.error("BOOT ERROR:", err);
+    throw err;
+}
