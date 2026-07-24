8efc2be7f1d9c35242ea94563ae24b869155f434
Pardhanvijay069 <pardhanvijay069@gmail.com>
2026-07-25 01:38:17 +0530

    fix: replace serverless-http wrapper with direct Express app export to resolve 504 function invocation timeouts on Vercel

--- api/index.js
+++ api/index.js
@@ -1,17 +1,5 @@
 require("dotenv").config();
 
-console.log("1. index.js loaded");
+const app = require("../src/app");
 
-try {
-    const serverless = require("serverless-http");
-    console.log("2. serverless-http loaded");
-
-    const app = require("../src/app");
-    console.log("3. app loaded");
-
-    module.exports = serverless(app);
-    console.log("4. export completed");
-} catch (err) {
-    console.error("BOOT ERROR:", err);
-    throw err;
-}
+module.exports = app;
